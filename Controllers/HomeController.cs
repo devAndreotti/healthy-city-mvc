aeea2ac0390ba2ffe9a2bea6472576d55bd09523
devAndreotti <ricardo.23.03.1749@gmail.com>
2024-09-02 10:04:17 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            try
+            {
+                DatabaseService dbs = new DatabaseService();
+                dbs.CadastraInteresse(cad);
+                return Json(new{status="OK"});
+            }
+            catch(Exception e)
+            {
+                _logger.LogError("REGISTRO DE ERROS CADASTRO" + e.Message);
+                return Json(new{status="ERR", mensagem="Erro no cadastro"});
+            }
+
+            //return View("Index",cad);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function cadastro() {
+    const parametros = {
+        Nome: $("#nome").val().trim(),
+        Email: $("#email").val().trim(),
+        Mensagem: $("#mensagem").val().trim(),
+    };
+
+    $.post("/Home/Cadastrar", parametros)
+        .done(function(data) {
+            const message = data.status === "OK" 
+                ? "<div class='alert alert-success'>Sua mensagem foi enviada!</div>"
+                : "<div class='alert alert-danger'>Erro ao enviar!</div>";
+            
+            $("#frm").hide().after(message);
+        })
+        .fail(function() {
+            $("#frm").after("<div class='alert alert-danger'>Erro no servidor</div>");
+        });
+}
+
+$(document).ready(function() {
+    $("#frmCadastro").submit(function(e) {
+        e.preventDefault();
+        cadastro();
+    });        
+});
